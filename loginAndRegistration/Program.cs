c326299b36e2ab8ed077376b500e6cb9c3108c06
neeraj3161 <neerajtripathi7447@gmail.com>
2022-08-25 17:26:24 +0530

    V1

--- loginAndRegistration/Program.cs
+++ loginAndRegistration/Program.cs
@@ -7,6 +7,35 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+
+
+//connecting to postgre sql using npgsql
+//first install npgsql using nuget package manager
+var conn_string = "Host = localhost; Username = postgres; Password = aaa@111;Database=MVC; Port = 5432";
+
+await using var conn = new NpgsqlConnection(conn_string);
+
+//opening the connection
+await conn.OpenAsync();
+
+var Createquery = "Create table if not exists mvc.user (user_id serial primary key, name varchar(50) not null, surname varchar(50), city varchar(500) not null,zip_code varchar(50) not null, phone_number varchar (50) unique not null)";
+
+//var to create a table
+
+var cmd = new NpgsqlCommand(Createquery, conn);
+
+//creating the table
+Console.WriteLine(await cmd.ExecuteNonQueryAsync());
+
+
+
+
+Console.WriteLine(conn.State);
+
+if (conn.State == System.Data.ConnectionState.Open) {
+    Console.WriteLine("Successfully connected to the database!!");
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -30,23 +59,7 @@ app.Run();
 
 
 
-//connecting to postgre sql using npgsql
-//first install npgsql using nuget package manager
-var conn_string = "Host = localhost; Username = postgres; Password = aaa@111;Database=MVC; Port = 5432";
 
-await using var conn = new NpgsqlConnection(conn_string);
-
-//opening the connection
-await conn.OpenAsync();
-
-var Createquery = "Create table if not exists user (user_id serial primary key, name varchar(50) not null, surname varchar(50), city varchar(500) not null,zip_code varchar(50) not null, phone_number varchar (50) unique not null)";
-
-//var to create a table
-
-var cmd = new NpgsqlCommand(Createquery,conn);
-
-//creating the table
-await using var reader = await cmd.Exe();
 
 
 
