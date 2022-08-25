bc7ba7b2893fbb33b56237441ecb332d1f02a6b3
neeraj3161 <neerajtripathi7447@gmail.com>
2022-08-25 17:05:15 +0530

    Add project files.

---
+++ loginAndRegistration/Program.cs
@@ -0,0 +1,52 @@
+using Npgsql;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+
+
+//connecting to postgre sql using npgsql
+//first install npgsql using nuget package manager
+var conn_string = "Host = localhost; Username = postgres; Password = aaa@111;Database=MVC; Port = 5432";
+
+await using var conn = new NpgsqlConnection(conn_string);
+
+//opening the connection
+await conn.OpenAsync();
+
+var Createquery = "Create table if not exists user (user_id serial primary key, name varchar(50) not null, surname varchar(50), city varchar(500) not null,zip_code varchar(50) not null, phone_number varchar (50) unique not null)";
+
+//var to create a table
+
+var cmd = new NpgsqlCommand(Createquery,conn);
+
+//creating the table
+await using var reader = await cmd.Exe();
+
+
+
