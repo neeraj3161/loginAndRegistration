4be70559d7685567200590c09b4b4d54afffefa3
neeraj3161 <neerajtripathi7447@gmail.com>
2022-08-26 13:50:42 +0530

    V1

---
+++ loginAndRegistration/Models/RegistrationClass.cs
@@ -0,0 +1,32 @@
+﻿using Npgsql;
+using System;
+using program;
+namespace loginAndRegistration.Models
+
+{
+    public class RegistrationClass
+    {
+
+
+        public static async Task insertData() {
+            NpgsqlConnection conn =   Program.StartConnection;
+            conn.Open();
+
+            if (conn.State == System.Data.ConnectionState.Open) {
+                Console.WriteLine("Connected to database");
+            }
+            
+            var insertQuery = "Insert into mvc.user(name,surname,city,zip_code,phone_number) values('Neeraj','Tripathi','Pune','411017','7447553161')";
+            await using var insert_cmd = new NpgsqlCommand(insertQuery, conn);
+            await insert_cmd.ExecuteNonQueryAsync();
+            //var command = "Select * from mvc.user";
+            // await using var cmd = new NpgsqlCommand(command, conn);
+            //using var reader =  await cmd.ExecuteReaderAsync();
+            // while (await reader.ReadAsync()) {
+            // Console.WriteLine(reader.GetString(0));
+            //}
+        }
+
+
+    }
+}
--- loginAndRegistration/Program.cs
+++ loginAndRegistration/Program.cs
@@ -1,65 +1,99 @@
 using Npgsql;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-
-
-//connecting to postgre sql using npgsql
-//first install npgsql using nuget package manager
-var conn_string = "Host = localhost; Username = postgres; Password = aaa@111;Database=MVC; Port = 5432";
-
-await using var conn = new NpgsqlConnection(conn_string);
-
-//opening the connection
-await conn.OpenAsync();
-
-var Createquery = "Create table if not exists mvc.user (user_id serial primary key, name varchar(50) not null, surname varchar(50), city varchar(500) not null,zip_code varchar(50) not null, phone_number varchar (50) unique not null)";
-
-//var to create a table
-
-var cmd = new NpgsqlCommand(Createquery, conn);
-
-//creating the table
-Console.WriteLine(await cmd.ExecuteNonQueryAsync());
-
-
-
-
-Console.WriteLine(conn.State);
-
-if (conn.State == System.Data.ConnectionState.Open) {
-    Console.WriteLine("Successfully connected to the database!!");
-}
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public class Program
+    {
+
+        private static string conn_string = "Host = localhost; Username = postgres; Password = aaa@111;Database=MVC; Port = 5432";
+
+        private static NpgsqlConnection conn = new NpgsqlConnection(conn_string);
+
+
+
+
+        public static NpgsqlConnection StartConnection { get {
+
+                //opening the connection
+                return conn;
+            } 
+        }
+        
+
+
+    private static void Main(string[] args)
+        {
+
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+
+
+            //var Createquery = "Create table if not exists mvc.user (user_id serial primary key, name varchar(50) not null, surname varchar(50), city varchar(500) not null,zip_code varchar(50) not null, phone_number varchar (50) unique not null)";
+
+            //var to create a table
+
+
+            //await using var cmd = new NpgsqlCommand(Createquery, conn);
+
+            //creating the table
+            //Console.WriteLine(await cmd.ExecuteNonQueryAsync());
+
+
+            //comment multiple lines using CTRL+K+C
+
+            //await using var conn = startConnecttionAsync().Result;
+
+            //var conn_string = "Host = localhost; Username = postgres; Password = aaa@111;Database=MVC; Port = 5432";
+
+            //await using var conn = new NpgsqlConnection(conn_string);
+
+
+            ////opening the connection
+            //await conn.OpenAsync();
+
+            conn.Open();
+
+            conn.Close();
+
+
+
+
+
+
+            Console.WriteLine(conn.State);
+
+            if (conn.State == System.Data.ConnectionState.Open)
+            {
+                Console.WriteLine("Successfully connected to the database!!");
+            }
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
-
-
-
-
-
-
-
